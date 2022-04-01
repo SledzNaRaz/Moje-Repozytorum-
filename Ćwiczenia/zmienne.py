c4d3344f7ef745a76189d1755fc7c853704aabc6
Ignacy Śledziński <rybenciusz@gmail.com>
2022-04-01 15:05:08 +0200

    Ignacy Śledziński
    Notatki

--- Ćwiczenia/zmienne.py
+++ Ćwiczenia/zmienne.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+from itertools import count
+
+
 i_liczba = 10.0 
 i_liczba += 1 #dodawanie plus jeden do wyniku 
 s_tekst1 = 'tekst1'
@@ -34,3 +38,95 @@ z_set2 = set([ 10, 2, 3, 7])
 print(z_set1.intersection(z_set2)) # część wspólna wzorów 
 print(z_set1.union(z_set2)) #zwrot wszystkich liczb 
 print(z_set1.difference(z_set2)) #wyświetla wyrazy które sie nie powtarzają 
+
+
+name = input("Podaj swoje imie")
+if len(name) <3: 
+    print("Podaj minimum 3 znaki")   
+else:
+    print("Witaj", name)
+
+
+
+
+a, b, c, d = 1, 1, 2, 2 #wylistowanie zmiennych 
+
+if (a and b and c and d > 1):
+    print("A")
+else:
+    print("B")
+
+
+counter = 1 
+while counter <= 10:
+    print (counter)
+    counter += 1 
+
+
+while True:
+    num = int(input("Podaj liczbe większą od 2 : "))
+    if num > 2:
+        print("Twoja liczba to ", num)
+        break
+    else:
+        print("To zła liczba")
+
+
+
+counter = 1 
+while counter <= 20: #pętla sie kończy kiedy counter będzie większe niż 20 
+    counter += 1 # dodaje sie jeden do counter 
+    if counter % 2 == 0:
+        continue  #pominięcie 
+    print(counter)
+
+
+counter = 1
+while counter <= 10:
+    print(counter)
+    if counter == 5:
+        exit() #wyjście z programu 
+    print("Następny")
+
+
+import time #wykorzystnie w przestrzeni backend sekundy od 1980 roku 
+
+for v in range(2,6):
+    print(v)
+    time.sleep(3) #przy każdym przejściu pętli 3 sekunty przestoju 
+
+a = time.gmtime() #data systemowa
+a0 = time.gmtime()[0] #pobieramy wartość która nas interesuje za pomocą indeksu 
+
+t1 = time.time()
+# Tu sie robi pętla 
+t2 = time.time() # to po pętli 
+r = t2 - t1 #czas w jakim się pętla wykonała 
+
+
+import time 
+a = time.strftime("%Y", time.gmtime()) #czas za pomocą stringa 
+
+
+a = datetime.datetime.now() #data systemowa 
+
+today = datetime.date.today() # sam rok 
+
+now = datetime.time(12, 3, 24 ,2222223323) #klasa czas i można sobie dać godzine jaką sie chce itp
+
+
+import csv 
+
+#odczyt
+n_line  = 0
+with open("testowy,csv", "r") as csvfile:
+    reader = csv.reader(csvfile) #przypisanie pliku do zmiennej 
+    for row in reader:
+        if n_line == 0:
+            print("s")
+        n_line += 1 
+    else:
+        print("Nazwa user to :")
+        n_line =+ 1 #autoinkrementacja 
+    
+    print("Ilość wierszy to : {n_line}" )
